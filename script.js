2e147694d77ac75e975135e45e6cb8e501da110f
Yuri007 <136291503+YuriKauan10@users.noreply.github.com>
2024-12-22 13:53:51 -0300

    feat: css quase pronto, criação de div tarefas para colocar div tarefa e botoes

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
 const pai = document.querySelector('#pai')
-const divTarefa = document.querySelector('#tarefa')
+const divTarefa = document.querySelector('#form')
 const tarefaInput = document.querySelector('#tarefaInput')
 const adicionar = document.querySelector('#adicionar')
-
+const botoes = document.querySelector('#botoes')
+const tarefas = document.querySelector('#tarefas')
+const divBranca = document.querySelector('#divBranca')
 
 
 adicionar.addEventListener('click', () => {
-    const botaoOk = document.createElement('button')
-    botaoOk.classList.add('botaoAdd')
-    const div = document.createElement('div')
-    div.classList.add('teste')
-
-    div.textContent = tarefaInput.value
-    pai.appendChild(div)
     
-    tarefaInput.value = ''
+    const div = document.createElement('div')
+    div.classList.add('tarefaAdicionada')
+    div.textContent = tarefaInput.value
+
+    divBranca.appendChild(div)
+
+    const taskConcluida = document.createElement('button')
+    taskConcluida.textContent = '✓'
+    taskConcluida.classList.add('taskConcluida')
+
+    const taskRemovida = document.createElement('button')
+    taskRemovida.textContent = '✕'
+    taskRemovida.classList.add('taskRemovida')
+    
+    botoes.appendChild(taskConcluida)
+    botoes.appendChild(taskRemovida)
+    
 })
