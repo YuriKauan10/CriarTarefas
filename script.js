0eee18513249ea091271ff03366b03a3ef916519
Yuri007 <136291503+YuriKauan10@users.noreply.github.com>
2024-12-22 12:43:47 -0300

    feat: tarefa adicionada

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-const tarefa = document.querySelector('#tarefa')
+const pai = document.querySelector('#pai')
+const divTarefa = document.querySelector('#tarefa')
+const tarefaInput = document.querySelector('#tarefaInput')
+const adicionar = document.querySelector('#adicionar')
+
+
+
+adicionar.addEventListener('click', () => {
+    const botaoOk = document.createElement('button')
+    botaoOk.classList.add('botaoAdd')
+    const div = document.createElement('div')
+    div.classList.add('teste')
+
+    div.textContent = tarefaInput.value
+    pai.appendChild(div)
+    
+    tarefaInput.value = ''
+})
