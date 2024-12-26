4969b00540055ca140a8e4ce78faeabcbe92038b
Yuri007 <136291503+YuriKauan10@users.noreply.github.com>
2024-12-26 17:19:03 -0300

    feat: não sei como meu cerebro conseguiu chegar a uma conclusão de resolver oque eu tava querendo , mas tá ai, adicionado estilos na divPrincipal ao apertar os botões e remoção da div

--- script.js
+++ script.js
@@ -8,63 +8,63 @@ const tarefas = document.querySelector('#tarefas')
 let numeroClasses = -1
 let arrayTarefas = []
 
+let divBranca
+let divP
+let p
+let divBotao
+let taskConcluida
+let taskRemovida
+
 adicionar.addEventListener('click', () => {
 
-    const divBranca = document.createElement('div')
+    divBranca = document.createElement('div')
     divBranca.classList.add('divBranca')
     tarefas.appendChild(divBranca)
 
-    const divP = document.createElement('div')
+    divP = document.createElement('div')
     divP.classList.add('divP')
     divBranca.appendChild(divP)
 
-    const p = document.createElement('p')
+    p = document.createElement('p')
     p.classList.add('tarefaAdicionada')
     p.textContent = tarefaInput.value
+
     if(tarefaInput.value.length === 0){
         p.textContent = '-Vazio-'
     } 
+
     divP.appendChild(p)
 
-    const divBotao = document.createElement('div')
+    divBotao = document.createElement('div')
     divBotao.classList.add('divBotao')
     divBranca.appendChild(divBotao)
 
-
-    const taskConcluida = document.createElement('button')
+    taskConcluida = document.createElement('button')
     taskConcluida.textContent = '✓'
     taskConcluida.classList.add('taskConcluida')
 
-    const taskRemovida = document.createElement('button')
+    taskConcluida.addEventListener('click', (e) => {
+        const {target} = e
+        const botaodiv = target.parentElement
+        const divprincipal = botaodiv.parentElement
+        divprincipal.classList.add('concluida')
+        divprincipal.firstElementChild.style.textDecoration = 'line-through'
+    })
+
+    taskRemovida = document.createElement('button')
     taskRemovida.textContent = '✕'
     taskRemovida.classList.add('taskRemovida')
     
-    divBotao.appendChild(taskConcluida)
-    divBotao.appendChild(taskRemovida)
-
-    let teste = 0
-    document.body.addEventListener('click', (e) => {
+    taskRemovida.addEventListener('click', (e) => {
         const {target} = e
-
-        if(target.className == 'taskConcluida'){
-            teste++
-            let paiBotao = target.parentElement
-            let paiDoPai = paiBotao.parentElement
-            let divParagrafo = paiDoPai.firstElementChild
-            console.log('eae')
-            divParagrafo.classList.add('pConcluido')
-            paiDoPai.classList.add('concluida')
-        }else if(target.className == 'taskRemovida'){
-           let botaoConcluido = target.previousSibling
-           let divBotaoSelect = botaoConcluido.parentElement
-           let paiDaDiv = divBotaoSelect.parentElement
-
-           try {
-            tarefas.removeChild(paiDaDiv)
-        
-           } catch (error) {if(!error){}}
-        }
+        const botaodiv = target.parentElement
+        const divprincipal = botaodiv.parentElement
+        tarefas.removeChild(divprincipal)
     })
 
+    divBotao.appendChild(taskConcluida)
+    divBotao.appendChild(taskRemovida)
+    arrayTarefas.push(divBranca)
 })
+
 
